5bedb0f4c90ef9fecee24211049f8335d88647ad
tunghdt91 <tunghdt91@gmail.com>
2013-07-28 21:08:24 +0700

    develop

--- protected/config/main.php
+++ protected/config/main.php
@@ -53,10 +53,10 @@ return array(
          */
         // uncomment the following to use a MySQL database
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=mms',
+            'connectionString' => 'mysql:host=localhost;dbname=MMS',
             'emulatePrepare' => true,
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
         ),
         'errorHandler' => array(
