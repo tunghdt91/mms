4b3d989d5e3d7bf10da5ab7611685bddd513a254
Pham Tri Thai <thaipt1012@gmail.com>
2013-08-03 17:10:26 +0700

    task#5010 tạo và import dữ liệu ban đầu của ban, chuc_vu_doan, dan_toc,...

---
+++ protected/data/danh_hieu.sql
@@ -0,0 +1,7 @@
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 1, 'Anh hùng lao động', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 2, 'Chiến sĩ thi đua', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 3, 'Anh hùng lao động', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 4, 'Nhà giáo ưu tú', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 5, 'Thầy thuốc ưu tú', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 6, 'Nghệ sĩ nhân dân', NULL);
+INSERT INTO danh_hieu (created_at, update_at, id, ten, mo_ta) VALUES ('2013-08-09 11:09:36', '2013-08-09 11:09:36', 7, 'Nghệ sĩ ưu tú', NULL);
