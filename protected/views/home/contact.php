f5b8d264681da4ef1601a176aa199a7469631c25
tunghdt91 <tunghdt91@gmail.com>
2013-08-13 11:08:53 +0700

    contact

---
+++ protected/views/home/contact.php
@@ -0,0 +1,13 @@
+<div class="clear2"></div>
+<div class="alert alert-block">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <h4>Thông báo !</h4>
+  Mọi ý kiến của thành viên xin gửi về địa chỉ hòm thư duy nhất (<i>Có ghi ở dưới</i>). Chúng tôi sẽ giải quyết các ý kiến của thành viên một cách sớm nhất. Nếu gửi thư xin vui lòng ghi rõ tên và địa chỉ chi tiết để chúng tôi có thể hỗ trợ.
+  <br/>
+</div>
+
+<h3>BQT Hệ thống quản lý đoàn viên</h3>
+Địa chỉ: 
+<br/>
+Số điện thoại: <span class="badge badge-info">043.....</span> - <span class="badge badge-info"> 09.....</span>
+<br/>Hòm thư điện tử<span class="badge badge-warning">abc@example.com</span> 
